f21b30cd5d6ffd2afdc96019043fd6bc7bb20d27
Mel Jack <melissa_jack@hotmail.co.uk>
2021-06-24 18:48:24 +0100

    Add basic HTML layout and list of JavaScript questions

---
+++ assets/javascript/script.js
@@ -0,0 +1,42 @@
+const questionsArray = [
+    {
+        question: "1. Which HTML element tag can you put JavaScript inside?",
+        a: "<javascript>",
+        b: "<script>",
+        c: "<js>",
+        d: "<java>",
+        correctAnswer: "b"
+    },
+    {
+        question: "2. Where should the JavaScript be referenced in the HTML?",
+        a: "The <head> section",
+        b: "The bottom of the <body> section",
+        c: "Below the <body> section",
+        d: "Either the <head> or the <body> section",
+        correctAnswer: "b"
+    },
+    {
+        question: "3. How do you add a comment in JavaScript?",
+        a: "// This is a comment",
+        b: "<!-- This is a comment -->",
+        c: "```This is a comment",
+        d: "** This is a comment **",
+        correctAnswer: "a"
+    },
+    {
+        question: "4. Which of these expressions is truthy?",
+        a: "3 > 5",
+        b: "'eight' == 8",
+        c: "'5' === 5",
+        d: "1 + 1 == 2",
+        correctAnswer: "d"
+    },
+    {
+        question: "5. Which of the following is an example of a JavaScript array?",
+        a: "var numbers = [1, 2, 3, 4]",
+        b: "var numbers = {1, 2, 3, 4}",
+        c: "var numbers = (1, 2, 3, 4)",
+        d: "var numbers = <1, 2, 3, 4>",
+        correctAnswer: "d"
+    }
+];
